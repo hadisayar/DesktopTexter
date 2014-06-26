13cf574231364920fca91edf5a37d6d7c576abc0
Hadi Eddy Sayar <sayar.hadi@gmail.com>
2014-06-26 12:48:51 -0400

    starting programming. send message

---
+++ TwilioTextClient/TwilioClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace TwilioTextClient
+{
+
+    class TwilioClient
+    {
+        //Find account sid and token on TWILIO account       
+        static string AccountSid = "";
+        static string AuthToken = "{{ }}";
+        string message = "This is an empty body";
+
+        bool SendMessage(string from, string to, string message)
+        {
+            this.message = message;
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+            var failure = twilio.SendMessage(from, to, this.message);
+
+            if (failure.ErrorMessage != null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
